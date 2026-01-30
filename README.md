6240f7873a65930caf0ae8d3b853b55a7ab2cd8b
PC-Ngumoha <pcngumoha@gmail.com>
2026-01-30 03:50:43 +0100

    Commence work on static version of website.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # HolyDays
+A very simple React.js application that helps you keep track of public holidays and other special days in any country of your choice.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const initialHolidays = [
+  {
+    "date": "2026-01-01",
+    "localName": "New Year's Day",
+    "name": "New Year's Day",
+    "countryCode": "NG",
+    "fixed": false,
+    "global": true,
+    "counties": null,
+    "launchYear": null,
+    "types": [
+      "Public"
+    ]
+  },
+  {
+    "date": "2026-04-03",
+    "localName": "Good Friday",
+    "name": "Good Friday",
+    "countryCode": "NG",
+    "fixed": false,
+    "global": true,
+    "counties": null,
+    "launchYear": null,
+    "types": [
+      "Public"
+    ]
+  },
+  {
+    "date": "2026-04-06",
+    "localName": "Easter Monday",
+    "name": "Easter Monday",
+    "countryCode": "NG",
+    "fixed": false,
+    "global": true,
+    "counties": null,
+    "launchYear": null,
+    "types": [
+      "Public"
+    ]
+  },
+  {
+    "date": "2026-05-01",
+    "localName": "Workers' Day",
+    "name": "Workers' Day",
+    "countryCode": "NG",
+    "fixed": false,
+    "global": true,
+    "counties": null,
+    "launchYear": null,
+    "types": [
+      "Public"
+    ]
+  },
+  {
+    "date": "2026-05-27",
+    "localName": "Children's Day",
+    "name": "Children's Day",
+    "countryCode": "NG",
+    "fixed": false,
+    "global": true,
+    "counties": null,
+    "launchYear": null,
+    "types": [
+      "Public"
+    ]
+  },
+  {
+    "date": "2026-06-12",
+    "localName": "Democracy Day",
+    "name": "Democracy Day",
+    "countryCode": "NG",
+    "fixed": false,
+    "global": true,
+    "counties": null,
+    "launchYear": null,
+    "types": [
+      "Public"
+    ]
+  },
+  {
+    "date": "2026-10-01",
+    "localName": "National Day",
+    "name": "National Day",
+    "countryCode": "NG",
+    "fixed": false,
+    "global": true,
+    "counties": null,
+    "launchYear": null,
+    "types": [
+      "Public"
+    ]
+  },
+  {
+    "date": "2026-11-01",
+    "localName": "National Youth Day",
+    "name": "National Youth Day",
+    "countryCode": "NG",
+    "fixed": false,
+    "global": true,
+    "counties": null,
+    "launchYear": null,
+    "types": [
+      "Public"
+    ]
+  },
+  {
+    "date": "2026-12-25",
+    "localName": "Christmas Day",
+    "name": "Christmas Day",
+    "countryCode": "NG",
+    "fixed": false,
+    "global": true,
+    "counties": null,
+    "launchYear": null,
+    "types": [
+      "Public"
+    ]
+  },
+  {
+    "date": "2026-12-28",
+    "localName": "Boxing Day",
+    "name": "Boxing Day",
+    "countryCode": "NG",
+    "fixed": false,
+    "global": true,
+    "counties": null,
+    "launchYear": null,
+    "types": [
+      "Public"
+    ]
+  }
+]
+
+
+export default function App() {
+  return <main>
+    <aside>
+      <header>
+        <nav className="logo">HolyDays</nav>
+        <div className="select-country">
+          <img src="https://flagsapi.com/US/flat/24.png" alt="Flag of the United states"/>
+          <select>
+            <option>United States</option>
+            <option>Nigeria</option>
+          </select>
+        </div>
       </header>
-    </div>
-  );
-}
+      <ul className="holiday-list">
 
-export default App;
+      </ul>
+    </aside>
+    <article> Main content</article>
+  </main>
+}
